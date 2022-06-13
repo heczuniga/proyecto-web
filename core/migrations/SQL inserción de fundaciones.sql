b8d8d1ae6a46a7cad770323620d98118dd802455
hector.zuniga.luarte@gmail.com <hector.zuniga.luarte@gmail.com>
2022-06-13 15:18:31 -0400

    Modificar las rutas de archivos para que los tome de media y se agregaron datos al modelo y los SQL respectivos para llenar la tabla de fundaciones de manera inicial.

---
+++ core/migrations/SQL inserción de fundaciones.sql
@@ -0,0 +1,61 @@
+
+insert into core_fundacion (
+	idFundacion,
+	nombreCorto,
+	nombreLargo,
+	descripcion,
+	sitioWeb,
+	imagen)
+values (
+	1,
+	'Ayuda a un peludo', 
+	'Fundación Ayuda a un peludo',
+	'Fundación sin fines de lucro dedicada a juntar a mascotas (perros y gatos) con personas que deseen adoptarlos.',
+	'ayudapeludo.cl',
+	'fundaciones/donaciones-ayudapeludo.jpg');
+
+insert into core_fundacion (
+	idFundacion,
+	nombreCorto,
+	nombreLargo,
+	descripcion,
+	sitioWeb,
+	imagen)
+values (
+	2,
+	'Fundación Abrázame', 
+	'Fundación Abrázame',
+	'Fundación sin fines de lucro dedicada a proteger a los niños, niñas y adolescentes residentes en hogares.',
+	'abrazame.cl',
+	'fundaciones/donaciones-abrazame.jpg');
+
+insert into core_fundacion (
+	idFundacion,
+	nombreCorto,
+	nombreLargo,
+	descripcion,
+	sitioWeb,
+	imagen)
+values (
+	3,
+	'Fundación Banigualdad', 
+	'Fundación Banigualdad',
+	'Fundación sin fines de lucro que financia, capacita y acompaña a pequeños emprendedores que buscan salir adelante gracias a sus negocios.',
+	'banigualdad.cl',
+	'fundaciones/donaciones-banigualdad.jpg');
+
+insert into core_fundacion (
+	idFundacion,
+	nombreCorto,
+	nombreLargo,
+	descripcion,
+	sitioWeb,
+	imagen)
+values (
+	4,
+	'Fundación Las Rosas', 
+	'Fundación Las Rosas',
+	'Fundación sin fines de lucro con la misión de acoger a las personas mayores más pobres del país, entregándoles amor, cariño y los cuidados necesarios para que vivan su vejez con la dignidad que todos merecemos.',
+	'fundacionlasrosas.cl',
+	'fundaciones/donaciones-lasrosas.jpg');
+
--- core/migrations/SQL inserción de productos.sql
+++ core/migrations/SQL inserción de productos.sql
@@ -10,7 +10,7 @@ values
 	'Bandana para perro floreada', 
 	'Bandana 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	5900,
-	'bandana1.jpg', 
+	'productos/bandana1.jpg', 
 	1);
 
 insert into core_producto
@@ -25,7 +25,7 @@ values
 	'Bandana para perro estilo leopardo', 
 	'Bandana 2: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	7900,
-	'bandana2.jpg', 
+	'productos/bandana2.jpg', 
 	1);
 
 insert into core_producto
@@ -40,7 +40,7 @@ values
 	'Bandana para perro corazones', 
 	'Bandana 3: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	4900,
-	'bandana3.jpg', 
+	'productos/bandana3.jpg', 
 	1);
 
 insert into core_producto
@@ -55,7 +55,7 @@ values
 	'Bandana floreada tropical', 
 	'Bandana 4: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	8900,
-	'bandana4.jpg', 
+	'productos/bandana4.jpg', 
 	1);
 
 insert into core_producto
@@ -70,7 +70,7 @@ values
 	'Bandana para perro Simpsons', 
 	'Bandana 5: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	12900,
-	'bandana5.jpg', 
+	'productos/bandana5.jpg', 
 	1);
 
 insert into core_producto
@@ -85,7 +85,7 @@ values
 	'Bandana con diseño ojos', 
 	'Bandana 6: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	14900,
-	'bandana6.jpg', 
+	'productos/bandana6.jpg', 
 	1);
 
 insert into core_producto
@@ -100,7 +100,7 @@ values
 	'Bandana con estilo Pet', 
 	'Bandana 7: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	5900,
-	'bandana7.jpg', 
+	'productos/bandana7.jpg', 
 	1);
 
 insert into core_producto
@@ -115,7 +115,7 @@ values
 	'Bandana con diseño cactus', 
 	'Bandana 8: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!', 
 	4900,
-	'bandana8.jpg', 
+	'productos/bandana8.jpg', 
 	1);
 
 insert into core_producto
@@ -131,7 +131,7 @@ values
 	'Correa 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	4900,
-	'correa1.jpg', 
+	'productos/correa1.jpg', 
 	2);
 
 insert into core_producto
@@ -147,7 +147,7 @@ values
 	'Correa 2: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	2900,
-	'correa2.jpg', 
+	'productos/correa2.jpg', 
 	2);
 
 insert into core_producto
@@ -163,7 +163,7 @@ values
 	'Correa 3: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	3900,
-	'correa3.jpg', 
+	'productos/correa3.jpg', 
 	2);
 
 insert into core_producto
@@ -179,7 +179,7 @@ values
 	'Correa 4: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	6900,
-	'correa4.jpg', 
+	'productos/correa4.jpg', 
 	2);
 
 insert into core_producto
@@ -195,7 +195,7 @@ values
 	'Correa 5: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	8900,
-	'correa5.jpg', 
+	'productos/correa5.jpg', 
 	2);
 
 insert into core_producto
@@ -211,7 +211,7 @@ values
 	'Correa 6: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	4900,
-	'correa6.jpg', 
+	'productos/correa6.jpg', 
 	2);
 
 insert into core_producto
@@ -227,7 +227,7 @@ values
 	'Correa 7: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	9900,
-	'correa7.jpg', 
+	'productos/correa7.jpg', 
 	2);
 
 insert into core_producto
@@ -243,7 +243,7 @@ values
 	'Correa 8: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim odio!
 ', 
 	10900,
-	'correa8.jpg', 
+	'productos/correa8.jpg', 
 	2);
 
 insert into core_producto
@@ -258,7 +258,7 @@ values
 	'Identificador con localizador', 
 	'Identificador 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	12900,
-	'identificador1.jpg', 
+	'productos/identificador1.jpg', 
 	3);
 
 insert into core_producto
@@ -273,7 +273,7 @@ values
 	'Identificador rosado para perro', 
 	'Identificador 2: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	11900,
-	'identificador2.jpg', 
+	'productos/identificador2.jpg', 
 	3);
 
 insert into core_producto
@@ -288,7 +288,7 @@ values
 	'Identificador planeta saturno', 
 	'Identificador 3: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	13900,
-	'identificador3.jpg', 
+	'productos/identificador3.jpg', 
 	3);
 
 insert into core_producto
@@ -303,7 +303,7 @@ values
 	'Identificador para personalizar', 
 	'Identificador 4: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	9900,
-	'identificador4.jpg', 
+	'productos/identificador4.jpg', 
 	3);
 
 insert into core_producto
@@ -318,7 +318,7 @@ values
 	'Identificador para registrar teléfono', 
 	'Identificador 5: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	8900,
-	'identificador5.jpg', 
+	'productos/identificador5.jpg', 
 	3);
 
 insert into core_producto
@@ -333,7 +333,7 @@ values
 	'Identificador color rosado', 
 	'Identificador 6: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	12900,
-	'identificador6.jpg', 
+	'productos/identificador6.jpg', 
 	3);
 
 insert into core_producto
@@ -348,7 +348,7 @@ values
 	'Identificador con nombre perro', 
 	'Identificador 7: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	11900,
-	'identificador7.jpg', 
+	'productos/identificador7.jpg', 
 	3);
 
 insert into core_producto
@@ -363,6 +363,6 @@ values
 	'Identificador rosado fuerte', 
 	'Identificador 8: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, deserunt quaerat laboriosam nostrum quod, molestiae nemo error aliquid cupiditate accusamus, quasi consectetur tenetur dolor nobis iure minus. Quos, enim', 
 	18900,
-	'identificador8.jpg', 
+	'productos/identificador8.jpg', 
 	3);
 
